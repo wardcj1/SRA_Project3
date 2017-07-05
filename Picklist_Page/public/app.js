1b2b8538822cf3120c2019048c4021da142c0202
chris ward <ward.christopherj@gmail.com>
2017-07-04 19:03:08 -0700

    adjusting look through css

--- Picklist_Page/public/app.js
+++ Picklist_Page/public/app.js
@@ -45,31 +45,15 @@ $(document).on("click", ".movetowatchlist", function() {
   getWatchlist();
 });
 
-
 // Functions
-
-// Load watchlist and render them to the screen
-// !!button needs to apply to entire div!! no break in rows!!
-// function getWatchlist() {
-//   $("#watchlist").empty();
-//   $.getJSON("/watchlist", function(data) {
-//     for (var i = 0; i < data.length; i++) {
-//       $("#watchlist").prepend("
-//         <div class='movetoportfolio' data-id='" + data[i]._id + "'>
-//         <div class=''><img src='images/" + data[i].brand + "' style='width:100px;height:100px;''></div><div>" + data[i].company + "</div><div>" + data[i].ticker + "</div></div>");
-//     }
-//     $("#watchlist").prepend("<tr><th>On the bench</th></tr>");
-//   });
-// }
-
 
 // Load watchlist and render them to the screen
 function getWatchlist() {
   $("#watchlist").empty();
   $.getJSON("/watchlist", function(data) {
     for (var i = 0; i < data.length; i++) {
-      $("#watchlist").prepend("<span class='tiles'><tr><th><button class='movetoportfolio' data-id='" + data[i]._id + "'><img src='images/" + data[i].brand + "' style='width:100px;height:100px;''></th><br><th>" + data[i].company + "</th><br><th>" + data[i].ticker +
-        "</button></th></tr></span>");
+      $("#watchlist").prepend("<span class='tiles'><tr><th><button class='movetoportfolio' data-id='" + data[i]._id + "'><img class='default-img' src='images/" + data[i].brand + "' style='width:100px;height:100px;''></th><br><th>" + data[i].company + "</th><br><th><p class='tickname'>" + data[i].ticker +
+        "</p></button></th></tr></span>");
     }
     $("#watchlist").prepend("<center><h2>On the Bench</h2></center>");
   });
@@ -80,12 +64,13 @@ function getPortfolio() {
   $("#portfolio").empty();
   $.getJSON("/portfolio", function(data) {
     for (var i = 0; i < data.length; i++) {
-      $("#portfolio").prepend("<span class='tiles'><tr><th><button class='movetowatchlist' data-id='" + data[i]._id + "'><img src='images/" + data[i].brand + "' style='width:100px;height:100px;''></th><br><th>" + data[i].company + "</th><br><th>" + data[i].ticker +
-        "</button></th></tr></span>");
+      $("#portfolio").prepend("<span class='tiles'><tr><th><button class='movetowatchlist' data-id='" + data[i]._id + "'><img class='default-img' src='images/" + data[i].brand + "' style='width:100px;height:100px;''></th><br><th>" + data[i].company + "</th><br><th><p class='tickname'>" + data[i].ticker +
+        "</p></button></th></tr></span>");
     }
     $("#portfolio").prepend("<center><h2>My Lineup (Max 20)</h2></center>");
   });
 }
+
 
 // function titleCase() {
 //   str = str.toLowerCase().split(' ');
@@ -95,7 +80,7 @@ function getPortfolio() {
 //   return str.join(' ');
 // }
 
-// Calling our functions
+// Calling functions
 getPortfolio();
 getWatchlist();
 
