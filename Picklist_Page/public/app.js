55822727491bdbbae68817ae068de7437c985f54
chris ward <ward.christopherj@gmail.com>
2017-07-04 19:56:03 -0700

    more css

--- Picklist_Page/public/app.js
+++ Picklist_Page/public/app.js
@@ -45,6 +45,27 @@ $(document).on("click", ".movetowatchlist", function() {
   getWatchlist();
 });
 
+// Click event to submit user stock selections from #portfolio
+// $("#userstock").on("click", function() {
+//   $.ajax({
+//     type: "POST",
+//     url: "/",
+//     dataType: "json",
+//     data: {
+//       company: $("#company").val().toUpperCase(),
+//       ticker: $("#ticker").val().toUpperCase(), 
+//       brand: $("#ticker").val().toUpperCase() + '.png',
+//       created: Date.now()
+//     }
+//   })
+//   .done(function(data) {
+//     console.log(data);
+//     getPortfolio();
+//   }
+//   );
+//   return false;
+// });
+
 // Functions
 
 // Load watchlist and render them to the screen
@@ -55,7 +76,7 @@ function getWatchlist() {
       $("#watchlist").prepend("<span class='tiles'><tr><th><button class='movetoportfolio' data-id='" + data[i]._id + "'><img class='default-img' src='images/" + data[i].brand + "' style='width:100px;height:100px;''></th><br><th>" + data[i].company + "</th><br><th><p class='tickname'>" + data[i].ticker +
         "</p></button></th></tr></span>");
     }
-    $("#watchlist").prepend("<center><h2>On the Bench</h2></center>");
+    $("#watchlist").prepend("<center><h2>On the Bench</h2></center><hr>");
   });
 }
 
@@ -67,7 +88,8 @@ function getPortfolio() {
       $("#portfolio").prepend("<span class='tiles'><tr><th><button class='movetowatchlist' data-id='" + data[i]._id + "'><img class='default-img' src='images/" + data[i].brand + "' style='width:100px;height:100px;''></th><br><th>" + data[i].company + "</th><br><th><p class='tickname'>" + data[i].ticker +
         "</p></button></th></tr></span>");
     }
-    $("#portfolio").prepend("<center><h2>My Lineup (Max 20)</h2></center>");
+    $("#portfolio").prepend("<center><h2>My Lineup (Max 20)</h2></center><hr>");
+    $("#portfolio").append("<hr><p><center><button type='submit' id='userstock' class='userstock-btn'>Submit</button></center></p>");
   });
 }
 
@@ -79,6 +101,29 @@ function getPortfolio() {
 //   }
 //   return str.join(' ');
 // }
+
+// Validation Rules
+// $(document).ready(function () {
+//             $("#addstock").validate({
+//                 rules: 
+//                 {
+//                     company:{
+//                     required:true,
+//                     },
+//                     ticker:{
+//                     required:true,  
+//                     },
+//                 },
+//                 messages:
+//                 {   
+//                     company:
+//                     { required:"Company name is required"
+//                     },
+//                     ticker:
+//                     { required:"Ticker Symbol is required",
+//                     },
+//                 }
+//             });
 
 // Calling functions
 getPortfolio();
